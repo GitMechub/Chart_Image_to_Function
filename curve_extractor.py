75f20e5c4d2205fce3f8068292520376bd13446e
MecHub <144913824+GitMechub@users.noreply.github.com>
2025-11-15 12:28:18 -0300

    Update curve_extractor.py

--- curve_extractor.py
+++ curve_extractor.py
@@ -103,28 +103,20 @@ def escalar_curva(contorno, x_fim, y_fim, x_ini=0., y_ini=0., eq_scale=True):
 
 if uploaded:
     imgp = Image.open(uploaded).convert("RGBA")
-    st.write(type(imgp))
     
     # Calculate the scaling factor
-    width_ratio = 800 / imgp.width
-    st.write(type(width_ratio))
-    height_ratio = 800 / imgp.height
-    scaling_factor = min(width_ratio, height_ratio)
-    new_width = int(imgp.width * scaling_factor)
-    new_height = int(imgp.height * scaling_factor)
-    imgp = imgp.resize((new_width, new_height), Image.Resampling.LANCZOS)
+    # width_ratio = 800 / imgp.width
+    # height_ratio = 800 / imgp.height
+    # scaling_factor = min(width_ratio, height_ratio)
+    # new_width = int(imgp.width * scaling_factor)
+    # new_height = int(imgp.height * scaling_factor)
+    # imgp = imgp.resize((new_width, new_height), Image.Resampling.LANCZOS)
 
     #max_size = (800, 800)
     #img = img.resize(max_size, Image.Resampling.LANCZOS)
 
     bg_img = np.array(imgp)  # RGBA
     rgb = bg_img[..., :3].copy()
-
-    #*
-    bufimgp = io.BytesIO()
-    imgp.save(bufimgp, format='PNG')
-    imgp_bytes = bufimgp.getvalue()
-    #*
 
     auto = sidebar.checkbox("Detect background color automatically", value=True)
     if auto:
@@ -153,7 +145,7 @@ if uploaded:
             fill_color="rgba(255, 0, 0, 0.0)",
             stroke_width=brush,
             stroke_color="#FF0000",
-            background_image=imgp_bytes,
+            background_image=imgp,
             update_streamlit=True,
             height=imgp.height,
             width=imgp.width,
@@ -330,6 +322,7 @@ with tab_about:
 st.sidebar.image(img_logo)
 st.sidebar.markdown(
     "[![YouTube](https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white)](https://www.youtube.com/@Mechub?sub_confirmation=1) [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/GitMechub)")
+
 
 
 
